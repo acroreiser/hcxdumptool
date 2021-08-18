6cbd0455d00ff91261c3c00c58810cbc409ff42d
Yaroslav Zviezda <acroreiser@gmail.com>
2021-08-18 23:32:35 +0300

    Revert "prepare to move from low level API to EVP"
    This reverts commit 789040045c67349f06e2be14fc6642871c4ceedc.

--- hcxdumptool.c
+++ hcxdumptool.c
@@ -570,11 +570,6 @@ if(rebootflag == true)
 		exit(EXIT_FAILURE);
 		}
 	}
-
-EVP_cleanup();
-CRYPTO_cleanup_all_ex_data();
-ERR_free_strings();
-
 if(errorcount != 0) exit(EXIT_FAILURE);
 if(totflag == true) exit(USER_EXIT_TOT);
 exit(EXIT_SUCCESS);
@@ -7641,9 +7636,9 @@ if(gpiostatusled > 0)
 		}
 	}
 
-
-ERR_load_crypto_strings();
-OpenSSL_add_all_algorithms();
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+SSL_library_init();
+#endif
 opensslversion = OpenSSL_version_num();
 opensslversionmajor = (opensslversion & 0x10000000L) >> 28;
 opensslversionminor = (opensslversion & 0x01100000L) >> 20;
